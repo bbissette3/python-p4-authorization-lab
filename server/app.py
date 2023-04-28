3d1681ae8cd5c56ac2412a929fced29d690bd007
Brian Bissette <bbissette3@yahoo.com>
2023-04-28 13:24:02 -0500

    lab complete:

--- server/app.py
+++ server/app.py
@@ -18,6 +18,19 @@ db.init_app(app)
 
 api = Api(app)
 
+@app.before_request
+def check_if_logged_in():
+    open_access_set = {
+        'clear', 
+        'article_list', 
+        'show_article', 
+        'login', 
+        'logout', 
+        'check_session'
+    }
+    if not session.get('user_id') and request.endpoint not in open_access_set:
+        return make_response('Unauthorized', 401)
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -85,14 +98,14 @@ class CheckSession(Resource):
         return {}, 401
 
 class MemberOnlyIndex(Resource):
-    
     def get(self):
-        pass
+        articles = Article.query.filter(Article.is_member_only == True).all()
+        return [article.to_dict() for article in articles], 200
 
 class MemberOnlyArticle(Resource):
-    
     def get(self, id):
-        pass
+        article = Article.query.filter_by(id=id).first()
+        return article.to_dict(), 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
